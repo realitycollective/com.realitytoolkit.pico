56ccadeabd3a312d90bcb0d6b762896ae18b200e
Dino Fejzagić <dino.f@live.de>
2023-03-29 21:10:24 +0200

    Update dependencies

--- com.unity.xr.picoxr/Editor/PXR_ManagerEditor.cs
+++ com.unity.xr.picoxr/Editor/PXR_ManagerEditor.cs
@@ -1,8 +1,14 @@
-﻿/************************************************************************************
- 【PXR SDK】
- Copyright 2015-2020 Pico Technology Co., Ltd. All Rights Reserved.
+﻿/*******************************************************************************
+Copyright © 2015-2022 PICO Technology Co., Ltd.All rights reserved.  
 
-************************************************************************************/
+NOTICE：All information contained herein is, and remains the property of 
+PICO Technology Co., Ltd. The intellectual and technical concepts 
+contained herein are proprietary to PICO Technology Co., Ltd. and may be 
+covered by patents, patents in process, and are protected by trade secret or 
+copyright law. Dissemination of this information or reproduction of this 
+material is strictly forbidden unless prior written permission is obtained from
+PICO Technology Co., Ltd. 
+*******************************************************************************/
 
 using System.Collections.Generic;
 using System.IO;
@@ -18,8 +24,6 @@ namespace Unity.XR.PXR.Editor
     [CustomEditor(typeof(PXR_Manager))]
     public class PXR_ManagerEditor : UnityEditor.Editor
     {
-        private GameObject fpsObject = null;
-
         public override void OnInspectorGUI()
         {
             GUI.changed = false;
@@ -28,28 +32,7 @@ namespace Unity.XR.PXR.Editor
             PXR_Manager manager = (PXR_Manager)target;
             PXR_ProjectSetting projectConfig = PXR_ProjectSetting.GetProjectConfig();
 
-            if (Camera.main != null)
-            {
-                if (!Camera.main.transform.Find("FPS"))
-                {
-                    fpsObject = Instantiate(Resources.Load<GameObject>("Prefabs/FPS"), Camera.main.transform, false);
-                    fpsObject.name = "FPS";
-                    fpsObject.SetActive(false);
-                }
-                else
-                {
-                    fpsObject = Camera.main.transform.Find("FPS").gameObject;
-                }
-            }
-            // Fps and Screen Fade
-            //manager.showFPS = EditorGUILayout.Toggle("Show FPS", manager.showFPS);
-
-            //manager.useDefaultFps = EditorGUILayout.Toggle("Use Default FPS", manager.useDefaultFps);
-            //if (!manager.useDefaultFps)
-            //{
-            //    manager.customFps = EditorGUILayout.IntField("    FPS", manager.customFps);
-            //}
-
+            //Screen Fade
             manager.screenFade = EditorGUILayout.Toggle("Open Screen Fade", manager.screenFade);
             if (Camera.main != null)
             {
@@ -82,19 +65,105 @@ namespace Unity.XR.PXR.Editor
             firstLevelStyle.wordWrap = true;
             var guiContent = new GUIContent();
             guiContent.text = "Eye Tracking";
-            guiContent.tooltip = "Before calling EyeTracking API, enable this option first, only for Neo 2 Eye device.";
-            manager.eyeTracking = EditorGUILayout.Toggle(guiContent, manager.eyeTracking);
+            guiContent.tooltip = "Before calling EyeTracking API, enable this option first, only for Neo3 Pro Eye , PICO 4 Pro device.";
+            projectConfig.eyeTracking = EditorGUILayout.Toggle(guiContent, projectConfig.eyeTracking);
+            manager.eyeTracking = projectConfig.eyeTracking;
             if (manager.eyeTracking)
             {
                 EditorGUILayout.BeginVertical("box");
                 EditorGUILayout.LabelField("Note:", firstLevelStyle);
-                EditorGUILayout.LabelField("EyeTracking is supported only on the Neo2 Eye");
+                EditorGUILayout.LabelField("Eye Tracking is supported only on Neo 3 Pro Eye , PICO 4 Pro");
                 EditorGUILayout.EndVertical();
             }
 
+            //face tracking
+            var FaceContent = new GUIContent();
+            FaceContent.text = "Face Tracking Mode";
+            manager.trackingMode = (FaceTrackingMode)EditorGUILayout.EnumPopup(FaceContent, manager.trackingMode);
+            if (manager.trackingMode == FaceTrackingMode.None) {
+                projectConfig.faceTracking = false;
+                projectConfig.lipsyncTracking = false;
+            }else if (manager.trackingMode == FaceTrackingMode.Hybrid)
+            {
+                projectConfig.faceTracking = true;
+                projectConfig.lipsyncTracking = true;
+            }
+            else if(manager.trackingMode == FaceTrackingMode.FaceOnly) {
+                projectConfig.faceTracking = true;
+                projectConfig.lipsyncTracking = false;
+            }else if (manager.trackingMode == FaceTrackingMode.LipsyncOnly)
+            {
+                projectConfig.faceTracking = false;
+                projectConfig.lipsyncTracking = true;
+            }
+            manager.faceTracking = projectConfig.faceTracking;
+            manager.lipsyncTracking = projectConfig.lipsyncTracking;
+
+            //hand tracking
+            var handContent = new GUIContent();
+            handContent.text = "Hand Tracking";
+            projectConfig.handTracking = EditorGUILayout.Toggle(handContent, projectConfig.handTracking);
+
             // content protect
             projectConfig.useContentProtect = EditorGUILayout.Toggle("Use Content Protect", projectConfig.useContentProtect);
-            manager.useRecommendedAntiAliasingLevel = EditorGUILayout.Toggle("Use Recommended MSAA", manager.useRecommendedAntiAliasingLevel);
+
+            //MRC
+            var mrcContent = new GUIContent();
+            mrcContent.text = "MRC";
+            projectConfig.openMRC = EditorGUILayout.Toggle(mrcContent, projectConfig.openMRC);
+            manager.openMRC = projectConfig.openMRC;
+            if (manager.openMRC == true)
+            {
+                EditorGUILayout.BeginVertical("frameBox");
+                string[] layerNames = new string[32];
+                for (int i = 0; i < 32; i++)
+                {
+                    layerNames[i] = LayerMask.LayerToName(i);
+                    if (layerNames[i].Length == 0)
+                    {
+                        layerNames[i] = "LayerName " + i.ToString();
+                    }
+                }
+                manager.foregroundLayerMask = EditorGUILayout.MaskField("foreground Layer Masks", manager.foregroundLayerMask, layerNames);
+                manager.backLayerMask = EditorGUILayout.MaskField("back Layer Masks", manager.backLayerMask, layerNames);
+                EditorGUILayout.EndVertical();
+            }
+            //Late Latching
+            projectConfig.latelatching = EditorGUILayout.Toggle("Use Late Latching", projectConfig.latelatching);
+            manager.lateLatching = projectConfig.latelatching;
+            if (Camera.main != null)
+            {
+                var head = Camera.main.transform;
+                if (head)
+                {
+                    var fade = head.GetComponent<PXR_LateLatching>();
+                    if (manager.lateLatching)
+                    {
+                        if (!fade)
+                        {
+                            head.gameObject.AddComponent<PXR_LateLatching>();
+                            Selection.activeObject = head;
+                        }
+                    }
+                    else
+                    {
+                        if (fade) DestroyImmediate(fade);
+                    }
+                }
+            }
+
+            // msaa
+            if (QualitySettings.renderPipeline != null)
+            {
+                EditorGUI.BeginDisabledGroup(true);
+                manager.useRecommendedAntiAliasingLevel = EditorGUILayout.Toggle("Use Recommended MSAA", manager.useRecommendedAntiAliasingLevel);
+                EditorGUI.EndDisabledGroup();
+                EditorGUILayout.HelpBox("A Scriptable Render Pipeline is in use,the 'Use Recommended MSAA' will not be used. ", MessageType.Info,true);
+            }
+            else
+            {
+                manager.useRecommendedAntiAliasingLevel = EditorGUILayout.Toggle("Use Recommended MSAA", manager.useRecommendedAntiAliasingLevel);
+            }
 
             if (GUI.changed)
             {
--- com.unity.xr.picoxr/Editor/PXR_SettingsEditor.cs
+++ com.unity.xr.picoxr/Editor/PXR_SettingsEditor.cs
@@ -1,8 +1,14 @@
-﻿/************************************************************************************
- 【PXR SDK】
- Copyright 2015-2020 Pico Technology Co., Ltd. All Rights Reserved.
+﻿/*******************************************************************************
+Copyright © 2015-2022 PICO Technology Co., Ltd.All rights reserved.  
 
-************************************************************************************/
+NOTICE：All information contained herein is, and remains the property of 
+PICO Technology Co., Ltd. The intellectual and technical concepts 
+contained herein are proprietary to PICO Technology Co., Ltd. and may be 
+covered by patents, patents in process, and are protected by trade secret or 
+copyright law. Dissemination of this information or reproduction of this 
+material is strictly forbidden unless prior written permission is obtained from
+PICO Technology Co., Ltd. 
+*******************************************************************************/
 
 using System.Collections;
 using System.Collections.Generic;
@@ -17,13 +23,16 @@ namespace Unity.XR.PXR.Editor
     {
         private const string StereoRenderingModeAndroid = "stereoRenderingModeAndroid";
         private const string SystemDisplayFrequency = "systemDisplayFrequency";
+        private const string OptimizeBufferDiscards = "optimizeBufferDiscards";
 
         static GUIContent guiStereoRenderingMode = EditorGUIUtility.TrTextContent("Stereo Rendering Mode");
-        static GUIContent guidisplayFrequency = EditorGUIUtility.TrTextContent("Display Refresh Rates");
+        static GUIContent guiDisplayFrequency = EditorGUIUtility.TrTextContent("Display Refresh Rates");
+        private static GUIContent guiOptimizeBuffer = EditorGUIUtility.TrTextContent("Optimize Buffer Discards(Vulkan)");
 
 
         private SerializedProperty stereoRenderingModeAndroid;
         private SerializedProperty systemDisplayFrequency;
+        private SerializedProperty optimizeBufferDiscards;
 
         void OnEnable()
         {
@@ -31,6 +40,8 @@ namespace Unity.XR.PXR.Editor
                 stereoRenderingModeAndroid = serializedObject.FindProperty(StereoRenderingModeAndroid);
             if (systemDisplayFrequency == null)
                 systemDisplayFrequency = serializedObject.FindProperty(SystemDisplayFrequency);
+            if (optimizeBufferDiscards == null)
+                optimizeBufferDiscards = serializedObject.FindProperty(OptimizeBufferDiscards);
         }
 
         public override void OnInspectorGUI()
@@ -39,27 +50,30 @@ namespace Unity.XR.PXR.Editor
                 return;
 
             serializedObject.Update();
-
+            EditorGUIUtility.labelWidth = 200.0f;
             BuildTargetGroup selectedBuildTargetGroup = EditorGUILayout.BeginBuildTargetSelectionGrouping();
             EditorGUILayout.Space();
 
             EditorGUILayout.BeginVertical(GUILayout.ExpandWidth(true));
             if (EditorApplication.isPlayingOrWillChangePlaymode)
             {
-                EditorGUILayout.HelpBox("PicoXR settings cannot be changed when the editor is in play mode.", MessageType.Info);
+                EditorGUILayout.HelpBox("PICO settings cannot be changed when the editor is in play mode.", MessageType.Info);
                 EditorGUILayout.Space();
             }
             EditorGUI.BeginDisabledGroup(EditorApplication.isPlayingOrWillChangePlaymode);
             if (selectedBuildTargetGroup == BuildTargetGroup.Android)
             {
                 EditorGUILayout.PropertyField(stereoRenderingModeAndroid, guiStereoRenderingMode);
-                EditorGUILayout.PropertyField(systemDisplayFrequency, guidisplayFrequency);
+                EditorGUILayout.PropertyField(systemDisplayFrequency, guiDisplayFrequency);
+                EditorGUILayout.PropertyField(optimizeBufferDiscards, guiOptimizeBuffer);
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("enableAppSpaceWarp"), new GUIContent("Application SpaceWarp"));
             }
             EditorGUI.EndDisabledGroup();
             EditorGUILayout.EndVertical();
             EditorGUILayout.EndBuildTargetSelectionGrouping();
 
             serializedObject.ApplyModifiedProperties();
+            EditorGUIUtility.labelWidth = 0f;
         }
     }
 }
